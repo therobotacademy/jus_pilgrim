bd5e01a77419c776b628ac9b3bd8e122fd8f95ec
roboticaeuropa <brjapon@psykia.com>
2016-11-06 22:10:40 +0100

    Cleaned control script 'start_stop.py'

--- start_stop.py
+++ start_stop.py
@@ -5,8 +5,6 @@
 import rospy
 from std_msgs.msg import Int32
 
-#Library of controller Maestro
-import maestro as m
 # Maestro channel assignment
 izq = 4
 dcho = 5
@@ -51,10 +49,6 @@ while not rospy.is_shutdown():
         wheel_stopped= izq
         write_left.publish(0)
         write_right.publish(1)
-        
-    #s= m.Controller()
-    #s.setTarget(wheel_moving,1)
-    #s.setTarget(wheel_stopped,0)
 
     print "Distance= ", dist        
     print "Rotating around wheel ", wheel_moving
